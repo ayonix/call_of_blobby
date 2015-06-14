4f8653978e0425048f9778c825c9d7841e69c832
Ayonix <git@leva.xyz>
2015-06-14 13:53:24 +0200

    Update to new version

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,137 @@
 extern crate piston;
 extern crate graphics;
-extern crate sdl2_window;
+extern crate glutin_window;
 extern crate opengl_graphics;
 
-use std::cell::RefCell;
 use piston::window::WindowSettings;
-use piston::input::Button;
-use piston::input::keyboard::Key;
 use piston::event::*;
-use graphics::{
-    Context,
-    ellipse,
-    RelativeTransform
-};
-use sdl2_window::Sdl2Window as Window;
+use graphics::*;
+use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
+use piston::input::{Button, Key};
+
+const gravity: f64 = 2.0;
+const groundHeight: f64 = 100.0;
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    player1: Player,
+    players: Vec<Player>
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let ref players = self.players;
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+
+            for p in players {
+                p.render(&c, gl);
+            }
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        for p in &mut self.players {
+            p.update()
+        }
+    }
 }
 
 struct Player {
     x: f64,
     y: f64,
+    radius: f64,
     color: [f32; 4],
+    velocity: f64,
+    jumping: bool
 }
 
-const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-
-impl App {
-    fn render(&mut self, _: &mut Window, args: &RenderArgs) {
-        // Set up a context to draw into.
-        let context = &Context::abs(args.width as f64, args.height as f64);
-
-        let center_context = &context.trans((args.width / 2) as f64, (args.height / 2) as f64)
-            .trans(-25.0, -25.0);
-
-        let circle = ellipse::circle(self.player1.x, self.player1.y, 50.0);
-        let color1 = self.player1.color;
-
-        self.gl.draw([0, 0, args.width as i32, args.height as i32], |_, gl| {
-            // Clear the screen.
-            graphics::clear(BLACK, gl);
-            graphics::ellipse(color1, circle, center_context.transform, gl);
-        });
+impl Player {
+    pub fn new(x: f64, y: f64, radius: f64, color: [f32; 4]) -> Player {
+        Player{x: x, y: y, radius: radius, color: color, velocity: 0.0, jumping: false}
     }
 
-    fn update(&mut self, _: &mut Window, args: &UpdateArgs) {
+    fn render(&self, c: &graphics::Context, gl: &mut GlGraphics) {
+        rectangle(self.color, rectangle::square(self.x, self.y, self.radius), c.transform, gl)
+    }
+
+    fn jump(&mut self) {
+        if !self.jumping {
+            self.velocity = -20.0; 
+            self.y += self.velocity;
+            self.jumping = true;
+        }
+    }
+
+    fn update(&mut self) {
+
+        if self.jumping {
+            self.y += self.velocity;
+            self.velocity += gravity;
+
+            if self.y >= groundHeight {
+                self.y = groundHeight;
+                self.jumping = false;
+            }
+        }
     }
 }
 
 fn main() {
-    // Create an SDL window.
+    let opengl = OpenGL::_3_2;
+
+    // Create an Glutin window.
     let window = Window::new(
-        OpenGL::_3_2,
-        WindowSettings::default()
+        opengl,
+        WindowSettings::new(
+            "Call of Blobby",
+            [200, 200]
+        )
+        .exit_on_esc(true)
     );
-    let window = RefCell::new(window);
 
     // Create a new game and run it.
     let mut app = App {
-        gl: GlGraphics::new(OpenGL::_3_2),
-        player1: Player {x: 0.0, y: 0.0, color: [0.5, 0.5, 0.5, 1.0]},
+        gl: GlGraphics::new(opengl),
+        players: vec![Player::new(0.0, groundHeight, 50.0, [1.0, 1.0, 0.0, 1.0]),
+                    Player::new(20.0, groundHeight, 50.0, [0.0, 0.5, 0.5, 1.0])]
     };
 
-    for e in events(&window) {
-        if let Some(Button::Keyboard(key)) = e.press_args() {
-            match key {
-                Key::Left => {
-                    app.player1.x -= 1.0;
-                },
-                Key::Right => {
-                    app.player1.x += 1.0;
-                },
-                Key::Up => {
-                    app.player1.y -= 1.0;
-                },
-                Key::Down => {
-                    app.player1.y += 1.0;
-                }
-                _ => {
-                    println!("Pressed {:?}", key);
-                }
-            }
-
-        }
-
+    for e in window.events() {
         if let Some(r) = e.render_args() {
-            app.render(&mut window.borrow_mut(), &r);
+            app.render(&r);
         }
 
         if let Some(u) = e.update_args() {
-            app.update(&mut window.borrow_mut(), &u);
+            app.update(&u);
+        }
+
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            if key == Key::D {
+                app.players[0].x += 20.0;
+            }
+            if key == Key::A {
+                app.players[0].x -= 20.0;
+            }
+            if key == Key::W {
+                app.players[0].jump();
+            }
+            if key == Key::L {
+                app.players[1].x += 20.0;
+            }
+            if key == Key::J {
+                app.players[1].x -= 20.0;
+            }
+            if key == Key::I {
+                app.players[1].jump();
+            }
         }
     }
 }
